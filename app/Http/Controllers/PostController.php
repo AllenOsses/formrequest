1e0d6f4d7c998935adba838890bc838957e2faef
Allen <allenn83@gmail.com>
2022-08-10 20:03:58 -0400

    validacion funcional , con token y dato por pantalla al enviar

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,7 +6,7 @@ use App\Http\Requests\PostRequest;
 
 class PostController extends Controller
 {
-    public function store(Request $request){
+    public function store(PostRequest $request){
         //esta validacion no debe estar aqui solo de practica
       dd($request->all());  
     }
